661bf7ce3801286eb3df46dabd72f29abdc405c7
azl0515 <1360069648@qq.com>
2020-04-18 15:19:50 +0800

    node-first commit

---
+++ public/template/precache-manifest.ee9b08f8d29f14b9293efeca416e39ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d46c479b45d6d64f0aaccf70c64c9db3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2520403d71fbd290176",
+    "url": "/static/css/main.97f8a107.chunk.css"
+  },
+  {
+    "revision": "f51947675eff3cec6c4b",
+    "url": "/static/js/2.1715e40b.chunk.js"
+  },
+  {
+    "revision": "890b83c3b503af9a5630afd9ff112890",
+    "url": "/static/js/2.1715e40b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2520403d71fbd290176",
+    "url": "/static/js/main.cb268f5d.chunk.js"
+  },
+  {
+    "revision": "9b585e4fdcec9150b394",
+    "url": "/static/js/runtime-main.6a36b6ec.js"
+  }
+]);
